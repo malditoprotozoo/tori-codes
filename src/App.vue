4af021a444a750e3346049a737c34a341c201b9a
Tori Rodriguez <virodriguez.fe@gmail.com>
2019-03-03 05:14:04 -0300

    replacing vue logos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img id="logo" alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -24,5 +24,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#logo {
+  max-width: 300px;
 }
 </style>
